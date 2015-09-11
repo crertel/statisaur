3deecb6022cb753a420f4eac9a9d8aeac91e3611
neerajt <neerajtandon25@gmail.com>
2015-09-11 19:57:21 -0400

    Add sum() and mean() to tests

--- lib/statisaur.ex
+++ lib/statisaur.ex
@@ -1,2 +1,33 @@
 defmodule Statisaur do
+
+  @doc """
+  Calculate the mean from a list of numbers
+
+  If list, coerce to list
+
+  ### Examples
+  iex>Statisaur.sum([1,3,5,7,9])
+  25
+  iex>Statisaur.sum([1,1])
+  2
+  iex>Statisaur.sum([0.5,0.5])
+  1.0
+
+  """
+  def sum(list) when is_list(list), do: Enum.sum(list)
+
+  @doc """
+  Calculate the mean from a list of numbers
+
+  If list, coerce to list
+
+  ### Examples
+  iex>Statisaur.mean([1,3,5,7,9])
+  5.0
+  iex>Statisaur.mean([0.1,0.2,0.6])
+  0.3
+
+  """
+  def mean(list) when is_list(list), do: sum(list)/length(list)
+
 end
--- test/statisaur_test.exs
+++ test/statisaur_test.exs
@@ -2,7 +2,22 @@ defmodule StatisaurTest do
   use ExUnit.Case
   doctest Statisaur
 
-  test "the truth" do
-    assert 1 + 1 == 2
+	test "sum([1,3,5,7,9])returns 25" do
+		assert 25 == Statisaur.sum([1,3,5,7,9])
+	end
+
+	test "sum([1,1])returns 2" do
+		assert 2 == Statisaur.sum([1,1])
+	end
+
+	test "mean([1,3,5,7,9]) returns 5" do
+		assert 5 == Statisaur.mean([1,3,5,7,9])
+	end
+
+  test "mean([0.1,0.2,0.6]) returns 0.3" do
+    assert 0.3 == Statisaur.mean([0.1,0.2,0.6])
   end
+
 end
+
+
