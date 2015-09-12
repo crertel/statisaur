d132e4a128257c1caea31f57b57d9607f5bcbbff
neerajt <neerajtandon25@gmail.com>
2015-09-11 23:30:24 -0400

    Add package info to mix.exs

--- mix.exs
+++ mix.exs
@@ -3,18 +3,17 @@ defmodule Statisaur.Mixfile do
 
   def project do
     [app: :statisaur,
-     version: "0.0.1",
-     elixir: "~> 1.1-beta",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+      version: "0.0.1",
+      elixir: "~> 1.1-beta",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps]
   end
 
   # Configuration for the OTP application
   #
   # Type `mix help compile.app` for more information
-  def application do
-    [applications: [:logger]]
+  def application do: []
   end
 
   # Dependencies can be Hex packages:
@@ -28,5 +27,13 @@ defmodule Statisaur.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     []
+  end
+
+  defp package do
+    [
+      contributors: ["Neeraj Tandon", "Chris Ertel"],
+      licenses: ["Public Domain (unlicense)"],
+      links: %{"GitHub" => "https://github.com/hawthornehaus/statisaur"}
+    ]
   end
 end
