1013ab22a1f46ff6b156e5caa93e108e22f2e6b3
neerajt <neerajtandon25@gmail.com>
2017-01-25 19:05:47 -0600

    Add pluck_all to load_csv

--- test/test_helper.ex
+++ test/test_helper.ex
@@ -14,10 +14,36 @@ defmodule Statisaur.Testhelper do
   1
 
   """
-  def pluck(list, n) when is_integer(n) and length(list) > n do
+  def pluck(list, n) when is_integer(n) and length(list) >= n do
     Enum.map(list, &Enum.at(&1, n))
   end
 
+  @doc """
+  Pluck all columns
+
+  ### Examples
+  iex>Statisaur.Testhelper.pluck_all([[1, 4, 3], [2, 3, 1]])
+  [[1, 2], [4, 3], [1, 3]]
+
+  """
+  def pluck_all(list, ncol, result) when ncol < 1 do
+    result = [result|pluck(list, ncol)]
+  end
+
+  def pluck_all(list, ncol) do 
+    result = pluck(list, ncol)
+  end
+
+  def pluck_all(list, ncol, result) do
+    result = [result|pluck(list, ncol)]
+    pluck_all(list, ncol - 1, result)
+  end
+
+  def pluck_contents(list) when is_list(list) do
+    ncol = length(list)
+    pluck_all(list, ncol)
+  end
+  
   @doc """
   Load csv
 
@@ -31,8 +57,11 @@ defmodule Statisaur.Testhelper do
   """
   def load_csv(file_path) when is_bitstring(file_path) do
     {:ok, contents} = File.read(file_path)
-    contents = contents |> String.split("\n") |> Enum.map(&String.split(&1, ","))
-    [pluck(contents, 0), pluck(contents, 1)]
+    [names, contents] = contents |>
+    String.split("\n") |>
+    Enum.map(&String.split(&1, ",")) |> 
+    pluck_contents() |>
+    Enum.map(fn(x) -> [hd(x), tl(x)] end)
 
   end
 end
