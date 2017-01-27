e05f986f6aa9861597c7c76c66f8e4295725604a
Geoff <geoffreyps@gmail.com>
2017-01-27 12:34:01 -0600

    Initialize combinatorics module

---
+++ lib/statisaur/combinatorics.ex
@@ -0,0 +1,5 @@
+defmodule Statisaur.Combinatorics do
+  @moduledoc """
+  Contains functions for analyzing finite descrete structures.
+  """
+end
---
+++ test/combinatorics_test.exs
@@ -0,0 +1,5 @@
+defmodule CombinatoricsTest do
+  use ExUnit.Case
+  doctest Statisaur.Combinatorics
+
+end
