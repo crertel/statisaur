b537b8c48341ba3609c1da9a9bf044d3b945055d
Geoff <geoffreyps@gmail.com>
2017-01-27 12:51:12 -0600

    Implement factorial function

--- lib/statisaur/combinatorics.ex
+++ lib/statisaur/combinatorics.ex
@@ -2,4 +2,19 @@ defmodule Statisaur.Combinatorics do
   @moduledoc """
   Contains functions for analyzing finite descrete structures.
   """
+  @doc ~S"""
+  Finds the factorial of a given non-negative integer.
+
+  The factorial of a non-negative integer n, is the product of all positive integers less than or equal to n.
+  
+  ### Example
+    The factorial of 5 is (5 * 4 * 3 * 2 * 1), or 120.
+
+      iex(1)> Statisaur.Combinatorics.factorial(5)
+      120
+  """
+  def factorial(n) when is_integer(n) do
+    Enum.reduce((1..n), 1, fn(x, acc) -> x * acc end)
+  end
+
 end
