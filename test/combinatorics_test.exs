a7e084b085b0dc26ddfe811c3bf29cd9fb5fd60f
Geoff <geoffreyps@gmail.com>
2017-01-27 12:39:37 -0600

    Add test case for factorial function happy path

--- test/combinatorics_test.exs
+++ test/combinatorics_test.exs
@@ -1,5 +1,10 @@
 defmodule CombinatoricsTest do
   use ExUnit.Case
   doctest Statisaur.Combinatorics
+  alias Statisaur.Combinatorics
+
+  test "factorial 5 returns 120" do
+    assert Combinatorics.factorial(5) == 120
+  end
 
 end
