b398a97aa09d803ae8f41daab4a7641ceaa5f9a2
neerajt <neerajtandon25@gmail.com>
2017-01-30 12:12:41 -0600

    Update load_csv output to list of tuples

--- test/helpers/test_helper.ex
+++ test/helpers/test_helper.ex
@@ -57,18 +57,18 @@ defmodule Statisaur.TestHelper do
 
   ### Examples
   iex> Statisaur.TestHelper.load_csv("./test/data/crickets.csv")
-  [["chirps_per_second", ["20.0", "16.0", "19.8", "18.4", "17.1", "15.5", "14.7",
-    "15.7", "15.4", "16.3", "15.0", "17.2", "16.0", "17.0", "14.4"]],
-     ["temperature_deg_f", ["88.6", "71.6", "93.3", "84.3", "80.6", "75.2", "69.7",
-       "71.6", "69.4", "83.3", "79.6", "82.6", "80.6", "83.5", "76.3"]]]
+  [{"chirps_per_second", ["20.0", "16.0", "19.8", "18.4", "17.1", "15.5", "14.7",
+    "15.7", "15.4", "16.3", "15.0", "17.2", "16.0", "17.0", "14.4"]},
+     {"temperature_deg_f", ["88.6", "71.6", "93.3", "84.3", "80.6", "75.2", "69.7",
+       "71.6", "69.4", "83.3", "79.6", "82.6", "80.6", "83.5", "76.3"]}]
   """
   def load_csv(file_path) when is_bitstring(file_path) do
     {:ok, contents} = File.read(file_path)
-    [names, contents] = contents |>
+    contents |>
     String.split("\n") |>
     Enum.filter( fn(x)-> (String.trim(x) |> String.length()) > 0 end ) |>
     Enum.map(&String.split(&1, ",")) |> 
     pluck_contents() |>
-    Enum.map(fn(x) -> [hd(x), tl(x)] end)
+    Enum.map(fn(x) -> {hd(x), tl(x)} end)
   end
 end
